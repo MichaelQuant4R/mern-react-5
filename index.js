ed4820b4b2ad447cc7ee288bbe400e72b8610761
MichaelQuant4R <michael.r2014@yahoo.co.uk>
2022-09-02 21:09:31 +0100

    build

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 const userRouter = require("./routes/user");
 
 
@@ -9,12 +10,19 @@ dotenv.config();
 const app = express();
 // app.use(cors());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://mern-react-cv.herokuapp.com/",
 }));
 app.use(express.json());
 
 
 app.use("/api/user", userRouter);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 
 
